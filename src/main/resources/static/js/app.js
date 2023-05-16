a3ec381a9b903de811a33cf03024f702a1307ce2
Matthew Hands <matthewjhands@users.noreply.github.com>
2023-05-17 00:38:25 +0100

    CRUD fuctionality done

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -83,24 +83,78 @@
   });
 
 
-  app.controller('notesController', function($scope){
+  app.controller('notesController', function($scope, $http){
 
     $scope.isEditCreateView = false;
+    $scope.selectedNote = null;
+
+    $scope.notes = {}; 
 
     $scope.newNoteView = function(){
         $scope.isEditCreateView = true;
+        $scope.selectedNote = {body: "", title: ""}; // note no id - this is set by server
     };
+
+    $scope.loadNotes = function() {
+        return $http.get("/api/notes").then(function(response){
+            $scope.notes = {}; // clear existing
+            response.data.forEach(element => {
+                $scope.notes[element.id] = element;
+            });
+            console.log("Notes obj loaded: ", $scope.notes);
+        });
+    }
 
     $scope.deleteNote = function (i) {
       var r = confirm("Are you sure you want to delete this note?");
       if (r == true){
-        //TODO delete the note
+        return $http.delete("/api/notes/" + $scope.selectedNote.id, $scope.selectedNote).then(function(response){
+            console.log("Got DELETE Response ", response);
+            $scope.loadNotes(); // refresh notes list
+            $scope.clearView(); 
+        });
       }
     };
 
-    $scope.viewNote = function(){
-        //TODO
+    $scope.viewNote = function(note){
+        // clone note so as not to update preview until save is clicked
+        noteClone = Object.assign({}, note); 
+        $scope.selectedNote = noteClone; 
+        $scope.isEditCreateView = true;
+        console.log("Viewing note ", note);
     }
+
+    $scope.saveNote = function() {
+        if($scope.selectedNote.id == null){
+            // no note id - this must be a new note
+            $http.post("/api/notes", $scope.selectedNote).then(function(response){
+                console.log("Got POST Response ", response);
+
+                // refresh notes list
+                $scope.loadNotes().then(function(){
+                    $scope.viewNote($scope.notes[response.data.id]); // reload note with id into view
+                });
+                
+            });
+        } else {
+            // update existing note
+            $http.put("/api/notes/" + $scope.selectedNote.id, $scope.selectedNote).then(function(response){
+                console.log("Got PUT Response ", response);
+
+                // refresh notes list
+                $scope.loadNotes().then(function(){
+                    $scope.viewNote($scope.notes[response.data.id]); // in theory shouldn't be necessary - belt and braces
+                });
+            });
+        }
+    }
+
+    $scope.clearView = function() {
+        $scope.isEditCreateView = false;
+        $scope.selectedNote = null;
+    }
+
+    $scope.loadNotes(); // load notes on page load
   });
 
 })();
